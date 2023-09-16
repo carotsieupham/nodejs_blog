f34b3fce719b84fd9b5245ca8ba1e6261cefecdc
unknown <tranminhtri10213@gmail.com>
2023-09-16 16:15:34 +0700

    add

--- src/index.js
+++ src/index.js
@@ -14,10 +14,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Template engine
 app.engine(
-    'hbs',
-    handlebars.engine({
-        extname: '.hbs',
-    }),
+          'hbs',
+          handlebars.engine({
+                    extname: '.hbs',
+          }),
 );
 app.set('view engine', 'hbs');
 
@@ -29,5 +29,5 @@ app.set('views', path.join(__dirname, 'resource/views'));
 route(app);
 // 127.0.0.1 - localhost:3000
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+          console.log(`Example app listening on port ${port}`);
 });

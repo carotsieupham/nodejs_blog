0ed0f8e42ef557f8129231185f39eefafa40e215
unknown <tranminhtri10213@gmail.com>
2023-10-05 19:09:20 +0700

    update 5/10

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-async function connect(){
-
-    try{
+async function connect() {
+    try {
         await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
-        console.log('Connect successfully')
-    }catch(error){
-        console.log('fail')
+            console.log('Connect successfully');
+    } catch (error) {
+        console.log(error);
     }
 }
-module.exports= {connect}
+module.exports = { connect };
--- src/routes/course.js
+++ src/routes/course.js
@@ -7,6 +7,7 @@ router.get('/create', courseController.create);
 router.post('/store', courseController.store);
 router.put('/:id', courseController.update);
 router.get('/:id/edit',courseController.edit)
+router.delete('/:id',courseController.delete)
 router.get('/:slug', courseController.show);
 
 module.exports = router;
